7fea7ee4bd7bf034ceca3d0aa8dc6018ceb72c71
ranjeevg <ranjeev.grewal1349@gmail.com>
2026-03-05 08:49:42 -0800

    DateTime extension methods
    Signed-off-by: ranjeevg <ranjeev.grewal1349@gmail.com>

--- WebAPI0002/Extensions/DateTimeExtensions.cs
+++ WebAPI0002/Extensions/DateTimeExtensions.cs
@@ -11,4 +11,46 @@ public static class DateTimeExtensions
     /// <returns></returns>
     public static DateOnly ToDateOnly(this DateTime dateTime)
         => new(year: dateTime.Year, month:dateTime.Month, day: dateTime.Day);
+
+    /// <summary>
+    /// An extension method extending the inbuilt AddDays() method.
+    /// </summary>
+    /// <param name="dateTime"></param>
+    /// <param name="days"></param>
+    /// <returns></returns>
+    public static DateTime SubtractDays(this DateTime dateTime, int days)
+        => dateTime.AddDays(days * -1);
+    
+    /// <summary>
+    /// Similar to the .AddDays() method inbuilt for DateTimes on C#.
+    /// </summary>
+    /// <param name="dateTime"></param>
+    /// <param name="days"></param>
+    /// <returns></returns>
+    public static DateTime SubtractDays(this DateTime dateTime, double days)
+        => dateTime.AddDays(days * -1);
+
+    /// <summary>
+    /// Returns the Monday of the week that
+    /// <paramref name="dateTime">dateTime</paramref>
+    /// falls on.
+    /// </summary>
+    /// <param name="dateTime"></param>
+    /// <remarks>
+    /// <i>
+    /// (Peering in to the source code for DateTimes, we note that
+    /// {Sunday : Saturday} is isomorphic to {0 : 6}.
+    /// <br /><br />
+    /// We use that to calculate the date value.)
+    /// </i>
+    /// </remarks>
+    public static int GetMondayOfWeek(this DateTime dateTime)
+    {
+        int MondayOfWeek;
+        
+        // Given that same isomrphism, Monday maps over to 1. 
+        
+
+        throw new NotImplementedException("");
+    }
 }
